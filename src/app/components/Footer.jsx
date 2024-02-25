b7fa7c3a9719eeeb63e0b5c42162648f4230c604
MOH. SYAHRUL AZIZ IMASTARA <sazizimastara@gmail.com>
2024-02-25 11:06:32 +0700

    change footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import GithubIcon from "../../../public/github-icon.svg";
+import LinkedinIcon from "../../../public/linkedin-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
+
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container px-24 py-12 flex justify-between">
         <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <div className="z-10">
+        <h5 className="text-xl text-white ">
+          Let&apos;s Connect
+        </h5>
+        {/* <p className="text-[#ADB7BE] mb-4 max-w-md">
+          {" "}
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
+        </p> */}
+        <div className="socials flex flex-row gap-2 py-4">
+          <Link href="https://github.com/AzizImastara">
+            <Image src={GithubIcon} alt="Github Icon" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/azizimastara/">
+            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          </Link>
+        </div>
       </div>
+      </div>
+      <div className="copyright container px-24 py-4 mb-4">
+        <p className="text-white">© 2024 Aziz Imastara</p>
+      </div>
+
     </footer>
+    
   );
 };
 
