7c688d43ca6e03aad41734177d5f799e3ce5364e
MOH. SYAHRUL AZIZ IMASTARA <sazizimastara@gmail.com>
2024-02-29 20:24:06 +0700

    add project description

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -8,56 +8,53 @@ const projectsData = [
   {
     id: 1,
     title: "Ticketing",
-    description: "Movie Website App",
+    description:
+      "Ticketing is an online-based film ticket booking application. In this Ticketing application, customers can order tickets, see the schedule of films currently showing, and a list of films that will be shown. Users in this application are divided into two roles, namely customer and admin. For the admin role, it is possible to make changes, add and delete film details and film schedules, while for the custommer role, you can order more than one ticket in one transaction.",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/AzizImastara/fe-tiketingapi",
-    previewUrl: "/",
+    previewUrl: "https://ticketings.netlify.app/",
   },
   {
     id: 2,
     title: "Zwallet",
-    description: "ewallet website app",
+    description:
+      "Zwallet is a transaction application that you can use to top up and transfer to fellow users of the zwallet application. Zwallet is built using NextJS.",
     image: "/images/projects/2.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/AzizImastara/zwallet-next",
-    previewUrl: "/",
+    previewUrl: "https://zwallet-next.netlify.app/",
   },
   {
     id: 3,
     title: "Panda Coffe",
-    description: "Panda coffe shop website app",
+    description:
+      "Public demands for convenience in every online activity, therefore the online coffee shop application was developed. With the availability of services via the internet, visitors can find out information about PandaCoffeShop's profile, products, customer service and place reservations. With this application, it is hoped that PandaCoffeShop's marketing program will be maximized",
     image: "/images/projects/3.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/justrinoo/pandaCoffee-frontend",
-    previewUrl: "/",
+    previewUrl: "https://panda-coffee-cip8cj9bf-riyaraa.vercel.app/",
   },
   {
     id: 4,
     title: "Jobrec",
-    description: "Online job seeker",
+    description:
+      "JobRec is an online job application application that aims to make it easier for applicants to find work and make it easier for recruiters to find applicants. The way this application works is that applicants can share their CV, portfolio or work experience in this application so that recruiters can see the applicant's CV or portfolio as well as the applicant's personal data, making it easier for recruiters to contact applicants.",
     image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
+    tag: ["All", "Web"],
     gitUrl: "https://github.com/fajriputra/frontend-jobrec",
-    previewUrl: "/",
+    previewUrl: "https://search-jobrect.netlify.app/",
   },
   {
     id: 5,
     title: "Ticketing Mobile",
-    description: "Authentication and CRUD operations",
+    description:
+      "Ticketing is an online-based film ticket booking application. In this Ticketing application, customers can order tickets, see the schedule of films currently showing, and a list of films that will be shown. Users in this application are divided into two roles, namely customer and admin. For the admin role, it is possible to make changes, add and delete film details and film schedules, while for the custommer role, you can order more than one ticket in one transaction.",
     image: "/images/projects/5.png",
-    tag: ["All", "Web"],
+    tag: ["All", "Mobile"],
     gitUrl: "https://github.com/AzizImastara/tiketing-native",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl:
+      "https://drive.google.com/file/d/1Np1izsVjtyjnx_DxaXi5WbByyw0n1JLE/view",
   },
 ];
 
