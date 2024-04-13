bb717eecb7bbcdef8df7208d843373a57883201d
MOH. SYAHRUL AZIZ IMASTARA <sazizimastara@gmail.com>
2024-04-13 12:19:11 +0700

    update content feat images

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,26 +15,23 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-2xl sm:text-2xl lg:text-4xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-secondary-500">
-              Hello, I&apos;m{" "}
+              Welcome to
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Ohto",
+                "My Personal Website",
                 1000,
-                "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
+                "Creative Portofolio",
                 1000,
               ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
+            <span>By Aziz Imastara</span>
           </h1>
         </motion.div>
         <motion.div
@@ -43,9 +40,9 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[300px] relative">
             <Image
-              src="/images/Arisu.png"
+              src="/images/gojocat.jpg"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
